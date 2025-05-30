c7e778c0f5caf3444350083262da404ef69644a8
pebbles005 <pebbles.dev.sulfur793@slmail.me>
2025-05-31 04:18:18 +0530

    made the fonts bold

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 0;     /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"CaskaydiaMono Nerd Font Mono:pixelsize=16"};
+static const char *fonts[] = {"CaskaydiaMono Nerd Font:bold:pixelsize=16"};
 static const char col_bg[] = "#191919";
 static const char col_fg[] = "#DBDBDB";
 static const char col_grey[] = "#7F8487";
